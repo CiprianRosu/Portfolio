1ec96499d0b9617da05dcc046ac9541ffe8318d8
Ciprian Andrei Rosu <cip.rosu@yahoo.com>
2022-11-04 15:29:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ import Work from './components/Work'
 import About from './components/About'
 import Skills from './components/Skills'
 import Footer from './components/Footer'
-import useLocalStorage from 'use-local-storage';
+
 
 const App = () => {
-  const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
-
-  const switchTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme)
-  }
-  console.log(window.location)
+  
   return (
-    <div className="app" data-theme={theme}>
-          <i onClick={switchTheme} className='fas fa-toggle-on'></i>
+    <div>
+          
     <Navbar/>
     <Header/>
     <Work/> 
